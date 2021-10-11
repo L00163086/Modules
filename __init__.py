1cb7ae3a2dfe579e3748a7ae24ab7e2584b93593
Chetan <91426296+L00163086@users.noreply.github.com>
2021-10-11 15:40:22 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,35 @@
+"""
+# -----------------------------------------------
+# File        : __init__.py.py
+# Created:    : 11-10-2021 15:25
+# Author      : Chetan Raga
+# Version     : v1.0.0
+# Licencing   : (c) 2021 Chetan raga,LYIT
+               AvaiLable under GNU Public License (GPL)
+# Description :
+   
+#------------------------------------------------
+"""
+
+if __name__ == '__main__;:
+    '''
+
+   Main method of application 
+
+
+
+   Linear programming only presented here wrt demo of lists
+
+
+
+   Parameters:
+
+    none
+
+
+
+   Returns:
+
+    none
+
+  '''
